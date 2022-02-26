c8e814093632eb09897c53b2d4652d8c0dd94de0
yongxinrobin <1101001900@qq.com>
2022-02-26 10:04:16 +0800

    Update import pandas as pd.py

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -1,23 +1,40 @@
 import pandas as pd
 
+#Check the Pandas Version
 print("Check the pandas version: " + pd.__version__)
    
-# 三个字段 name, site, age
+#Three field name, site, age
 nme = ["Google", "Runoob", "Taobao", "Wiki"]
 st = ["www.google.com", "www.runoob.com", "www.taobao.com", "www.wikipedia.org"]
 ag = [90, 40, 80, 98]
    
-# 字典
+#Dictionary
 dict = {'name': nme, 'site': st, 'age': ag}
      
 df = pd.DataFrame(dict)
 
 print(df.to_string)
  
-# 保存 dataframe
+#Save dataframe
 #df.to_csv('site.csv')
 print("--------------------------------")
 csvOutput = pd.read_csv('D:/Python/site.csv');
 print(csvOutput.to_string)
 print("--------------------------------")
+
+#Filter the duplicate records by using Pandas
+print("************************************")
+print("#Filter the duplicate records by using Pandas")
+person = {
+    "name":['Amy','Mike','Amy','Jeff'],
+    "age":[21,40,21,50]
+}
+dp=pd.DataFrame(person)
+# if it's duplicated(), return: True，else: False
+print(dp.duplicated())
+#If want to delete the duplicate records, use method: drop_duplicates() 
+print(dp.drop_duplicates(inplace=True))
+print(dp)
+print("************************************")
+
  
